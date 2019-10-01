ed51f569c53fb4634a315181a239d36a003f4c43
Augusto Sandim <augustosandim@gmail.com>
2019-10-01 09:05:57 -0400

    Fix transaction sum value

--- src/utils/calculateAmount.js
+++ src/utils/calculateAmount.js
@@ -1,15 +1,10 @@
-export const calculateTotal = (transactions) => {
+export const calculateTotal = transactions => {
   if (!transactions) return 0;
 
   return transactions.reduce((acc, item) => {
     return (
       Number(acc) +
-      Number(
-        item.transactionValue
-          .replace('.', '')
-          .replace('R$', '')
-          .replace(',', '.'),
-      )
+      Number(item.transactionValue.replace(/[^0-9,-]+/g, '').replace(',', '.'))
     );
   }, 0);
 };
